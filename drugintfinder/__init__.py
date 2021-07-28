a756db99b7efd499d8cc91dac6c719fa1e0b3ce9
Bruce Schultz <bschultz013@gmail.com>
2021-07-28 12:48:04 +0200

    docs: update homepage of documentation

--- drugintfinder/__init__.py
+++ drugintfinder/__init__.py
@@ -1,4 +1,15 @@
-"""Top-level package for Druggable Interactor Finder."""
+"""A pipeline for identifying the most notable druggable interactors of a target within a e(BE:L) generated
+Knowledge Graph.
+
+This package is designed for identifying the direct interactors of a specified node within an e(BE:L) generated
+Knowledge Graph. The starting node can be filtered by given protein modifications and the type of interactors
+(e.g. gene, RNA, protein) can also be controlled. Additioanlly, this package also can identify those interactors
+that are druggable and provide a table of useful statistics about each drug/target combination that can be used for
+ranking these pairs.
+
+To get started with the druggable interactor finder, you can install it using:
+``pip install drugintfinder``
+"""
 
 from drugintfinder import defaults
 
